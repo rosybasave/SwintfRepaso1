cc40067d07dce312efb721ffc307ddf546827ace
rosy basave <rosybasave80@gmail.com>
2025-01-21 11:53:25 -0600

    Initial Commit

---
+++ SwintfRepaso1/SwintfRepaso1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwintfRepaso1App.swift
+//  SwintfRepaso1
+//
+//  Created by Rosy Ilda Basave Torres on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwintfRepaso1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
